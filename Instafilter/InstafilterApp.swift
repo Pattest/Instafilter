b443885efe04f4b1bd79bc872f76fd4979e6a139
Pattest <baptistecadoux@gmail.com>
2022-02-28 10:22:19 +0100

    first commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Baptiste Cadoux on 28/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
