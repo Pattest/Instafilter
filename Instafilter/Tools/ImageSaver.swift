0f32a838977b3e63980f8b4358b452da6313cc81
Pattest <baptistecadoux@gmail.com>
2022-03-04 13:52:35 +0100

    feat(Day 66): Project 13, part five
    - Customizing our filter using confirmationDialog()
    - Saving the filtered image using UIImageWriteToSavedPhotosAlbum()

---
+++ Instafilter/Tools/ImageSaver.swift
@@ -0,0 +1,26 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Baptiste Cadoux on 02/03/2022.
+//
+
+import UIKit
+import PhotosUI
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveComplete), nil)
+    }
+
+    @objc func saveComplete(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
